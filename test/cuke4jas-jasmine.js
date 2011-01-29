3944474f1e257bf2d5b204acf474b5d0e0b397af
vinodkd <vinod@vinodkd.org>
2011-01-28 18:15:34 -0600

    generated this using convert, but willl be used to test cuke4jas as well

---
+++ test/cuke4jas-jasmine.js
@@ -0,0 +1,11 @@
+feature('Convert cuke spec to jasmine template', function(){
+	scenario('Test basic cuke spec', function(){
+		given('basic cuke spec', function(){
+		});
+		when('I convert it using cuke4jas', function(){
+		});
+		then('I should see jasmine template 1', function(){
+		});
+		
+	});
+});
